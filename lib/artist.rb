7c4c4aad239b453838a482119a1559ef6760dfbc
Evan Ratzlaff <ratzlaff92@gmail.com>
2019-07-07 14:45:31 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -43,7 +43,7 @@ class Artist
 
   def print_songs #instance method iterates through @songs of an instance of Artist
     self.songs.each do |song|
-      @songs.length 
+      song.length 
     end 
     puts @songs.length
   end
